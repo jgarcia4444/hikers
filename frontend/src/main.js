208a73248f5f17734e94aa95e13d4bcba8c83d72
github username <github email address>
2020-06-22 17:17:46 -0700

    basic styling setup to contain the hikes within a specified window. Also event listener added document to listen for when the document has fully loaded.

---
+++ frontend/src/main.js
@@ -0,0 +1,11 @@
+const BASE_URL = 'http://localhost:3000'
+const HIKES_URL = BASE_URL + '/hikes'
+
+
+function fetchHikes() {
+    
+}
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    fetchHikes();
+})
