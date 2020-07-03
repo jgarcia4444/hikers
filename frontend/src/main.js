af468c37d11ac273e169ee50d6547e81ba5e06ea
Jake <github email address>
2020-07-03 13:34:01 -0700

    backend now has a User  model and migration to go along with it.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -365,3 +365,11 @@ class Comment {
         })
     }
 }
+
+class User {
+    constructor(id, firstName, lastName) {
+        this.id = id
+        this.firstName = firstName
+        this.lastName = lastName
+    }
+}
